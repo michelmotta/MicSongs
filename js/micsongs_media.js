0a4de04ef3770fdc2832f32c5e7b5af754242aa9
michelmotta <michelmottadasilva@gmail.com>
2017-07-18 16:02:21 -0400

    Add javascript

---
+++ js/micsongs_media.js
@@ -0,0 +1,30 @@
+jQuery(function($) {
+  $(document).ready(function(){
+  	$('#micsong_upload_button').click(open_media_window);
+  });
+
+  function open_media_window() {
+  	if (this.window === undefined) {
+     	this.window = wp.media({
+        title: 'Inserir Música',
+        library: {
+        	type: 'audio/mpeg'
+        },
+        multiple: false,
+        button: {
+          text: 'Insert'
+        }
+      });
+
+    	var self = this; // Needed to retrieve our variable in the anonymous function below
+      this.window.on('select', function() {
+        var first = self.window.state().get('selection').first().toJSON();
+        console.log(first.url);
+        $('#song-url').val(first.url);
+        //wp.media.editor.insert('[myshortcode id="' + first.id + '"]');
+      });
+    }
+    this.window.open();
+    return false;
+  }
+});
