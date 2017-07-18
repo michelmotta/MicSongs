ea5773628c3c4eec2acd86413e1e69d086ef914f
michelmotta <michelmottadasilva@gmail.com>
2017-07-18 10:30:20 -0400

    Creation index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: MicSongs
+ * Plugin URI: https://github.com/michelmotta/MicSongs
+ * Description: It's pretty wordpress plugin developed to upload and display mp3 songs using shortcodes. 
+ * Version: 0.1
+ * Author: Michel Motta da Silva
+ * Author URI: https://github.com/michelmotta
+ * License: GPL2
+ */ 
+
+
+class micsongs
+{
+
+}
