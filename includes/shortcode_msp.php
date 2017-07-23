3ebc4f0eaa69a1624f12fe1fe57cc0227e7a2b63
michelmotta <michelmottadasilva@gmail.com>
2017-07-22 22:00:16 -0400

    Alterações frontend dos shortcodes

--- includes/shortcode_msp.php
+++ includes/shortcode_msp.php
@@ -15,12 +15,26 @@
       $count = 0;
       foreach ($posts as $p) :
       $songurl = get_post_meta( $p->ID, 'song-url', true );
+      $songDescription = get_post_meta($p->ID, 'song-description', true); 
+      $allowDownload = get_post_meta($p->ID, 'song-allow-download', true);
     ?>
       <li class="<?php if($count == 0) echo "active"; ?>">
         <a href="<?php echo $songurl; ?>">
           <?php echo $p->post_title; ?>
         </a>
-        <a href="<?php echo $songurl;?>" class="download-song block" data-songid="<?php echo $p->ID ;?>" data-songname="<?php echo $p->post_title;?>" style="float: right;" download>Baixar</a>
+        <a href="#" class="block" style="float: right;" data-toggle="collapse" data-target="#msp<?php echo $count . $p->ID; ?>"><i class="fa fa-info" aria-hidden="true"></i> Ver Mais</a>
+        <?php
+          if($allowDownload == "yes"){
+        ?>    
+          <a href="<?php echo $songurl;?>" class="download-song block" data-songid="<?php echo $p->ID ;?>" data-songname="<?php echo $p->post_title;?>" style="float: right;" download>
+            <i class="fa fa-download" aria-hidden="true"></i> Baixar
+          </a>
+        <?php
+          }
+        ?>
+        <div id="msp<?php echo $count . $p->ID; ?>" class="collapse songDescription">
+          <?php echo $songDescription; ?>
+        </div> 
       </li>
     <?php  
       $count++;
--- includes/shortcode_mss.php
+++ includes/shortcode_mss.php
@@ -3,13 +3,28 @@ if($musica){
   $songTitle = get_the_title( $musica );
   $songurl = get_post_meta($musica, 'song-url', true); 
   $songDescription = get_post_meta($musica, 'song-description', true); 
+  $allowDownload = get_post_meta($musica, 'song-allow-download', true);
 ?>
-  <p id="song-name-mss"><?php echo $songTitle;?></p>
-  <p id="song-description-mss"><?php echo $songDescription;?></p>
+<div class="micsongs-player">
   <audio preload="auto" tabindex="0" controls="" style="width: 100%;">
     <source src="<?php echo $songurl;?>" type="audio/mpeg">
     Your browser does not support the audio tag.
   </audio> 
-  <p id="song-download-mss" ><a href="<?php echo $songurl;?>" class="download-song" data-songid="<?php echo $musica;?>" data-songname="<?php echo $songTitle;?>" style="float: right;" download>Baixar</a></p>
+  <p id="song-download-mss"><?php echo $songTitle;?>
+    <a href="#" class="block" style="float: right;" data-toggle="collapse" data-target="#mss<?php echo $musica; ?>"><i class="fa fa-info" aria-hidden="true"></i> Ver Mais</a>
+    <?php
+      if($allowDownload == "yes"){
+    ?>    
+      <a href="<?php echo $songurl;?>" class="download-song block" data-songid="<?php echo $musica;?>" data-songname="<?php echo $songTitle;?>" style="float: right;" download>
+        <i class="fa fa-download" aria-hidden="true"></i> Baixar
+      </a>
+    <?php
+      }
+    ?>
+  </p>
+  <div id="mss<?php echo $musica; ?>" class="collapse songDescription">
+    <?php echo $songDescription; ?>
+  </div> 
+</div>  
 <?php
 }
